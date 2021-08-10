5e667b7ba3646fa5f45e484f1202826b8f25f043
Syed Shehroz Ali <sshehroza13@gmail.com>
2021-08-10 23:07:30 +0500

    Included Bootstrap5 & Updated quiz difficulty level

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { stat } from 'fs';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
 import gameFinished from './components/gameFinished';
+import './App.css'
 
 function App() {
 
@@ -33,28 +33,64 @@ function App() {
 
       // Continue game
       if (state.round < TotalRounds) {
-        let CorrectAns = state.num1 + state.num2;   // Adds two numbers
-        if (parseInt(state.ans) == CorrectAns) {
-          // Call setState() -> Update the question
-          setState({
-            round: state.round + 1,
-            num1: Math.floor((Math.random() * 10) + 1),
-            num2: Math.floor((Math.random() * 10) + 1),
-            ans: "",
-            score: state.score + 1
-          });
 
-        } else {
-          // Call setState() -> Update the question
-          setState({
-            round: state.round + 1,
-            num1: Math.floor((Math.random() * 10) + 1),
-            num2: Math.floor((Math.random() * 10) + 1),
-            ans: "",
-            score: state.score
-          });
-        };
-      };
+        // For all rounds that comes after 4 (Increases Difficulty Level)
+        if (state.round > 4) {
+          let CorrectAns = state.num1 + state.num2;
+          if (parseInt(state.ans) == CorrectAns) {
+
+            // Call setState() -> Update the question (Increase Difficulty Level Also)
+            setState({
+              round: state.round + 1,
+              num1: Math.floor((Math.random() * 100) + 1),
+              num2: Math.floor((Math.random() * 100) + 1),
+              ans: "",
+              score: state.score + 1
+            });
+          }
+
+          else {
+            setState({
+              round: state.round + 1,
+              num1: Math.floor((Math.random() * 10) + 1),
+              num2: Math.floor((Math.random() * 10) + 1),
+              ans: "",
+              score: state.score
+            });
+
+
+          } //
+          } //
+
+        // For all rounds that comes or equal to 4 (Normal Difficulty Level)
+        if (state.round <= 4) {
+          let CorrectAns = state.num1 + state.num2;   // Adds two numbers
+          if (parseInt(state.ans) == CorrectAns) {
+            // Call setState() -> Update the question
+            setState({
+              round: state.round + 1,
+              num1: Math.floor((Math.random() * 10) + 1),
+              num2: Math.floor((Math.random() * 10) + 1),
+              ans: "",
+              score: state.score + 1
+            });
+
+          } else {
+            // Call setState() -> Update the question
+            setState({
+              round: state.round + 1,
+              num1: Math.floor((Math.random() * 10) + 1),
+              num2: Math.floor((Math.random() * 10) + 1),
+              ans: "",
+              score: state.score
+            });
+
+
+          } //
+
+        };//
+      
+      }; //
 
       // When Final Round is Finished
       if (state.round == TotalRounds) {
@@ -75,19 +111,27 @@ function App() {
 
         // TODO
 
-      };
-    };
-  };
+      };  //
+
+    }; //
+
+  }; //
 
   // Main Quiz Window
   return (
-    <div>
-      <h1>Round: {state.round}</h1>
-      <div>{state.num1} + {state.num2}</div>
-      <input value={state.ans} onChange={updateAnswer} onKeyPress={checkAnswer}
-        type="text" autoFocus />
-      <div>Score: {state.score}</div>
-    </div>
+    <body className="d-flex justify-content-center align-items-center">
+      <div className="container">
+        <div className="row d-flex flex-column justify-content-center align-items-center">
+          <div className="col-12 d-flex flex-column justify-content-center align-items-center">
+            <h1 className="display-4 text-dark mb-5">Round: {state.round}</h1>
+            <div className="mb-3 fs-4">Ques. {state.num1} + {state.num2} ?</div>
+            <input value={state.ans} onChange={updateAnswer} onKeyPress={checkAnswer}
+              type="text" autoFocus />
+            <div className="mt-5 fw-bold">Score: {state.score}</div>
+          </div>
+        </div>
+      </div>
+    </body>
   );
 }
 
