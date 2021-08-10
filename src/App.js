5dbcc734d85373c695508a44f241082af6a3cbc9
Syed Shehroz Ali <sshehroza13@gmail.com>
2021-08-10 19:43:47 +0500

    Added Round counter with scoreboard display

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { stat } from 'fs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
+import gameFinished from './components/gameFinished';
 
 function App() {
 
+  // Total Number of Rounds in Quiz
+  const TotalRounds = 10;
+
   // Declare React state
   const [state, setState] = React.useState({
+    round: 1,
     num1: 1,
     num2: 2,
-    ans: ""
-  })
+    ans: "",
+    score: 0
+  });
 
   // Update the input field what ever the user has typed in
   function updateAnswer(event) {
@@ -21,31 +28,65 @@ function App() {
 
   // Check the user submitted Answer
   function checkAnswer(event) {
+
     if (event.key == "Enter") {
-      
-      let CorrectAns = state.num1 + state.num2;   // Adds two numbers
-      if (parseInt(state.ans) == CorrectAns) {
-        console.log("Answer is Correct!");
 
-        // Call setState()
-        setState({
-          num1: 5,
-          num2: 11,
-          ans: ""
-        });
-        
-      } else {
-        console.log("Answer is Wrong!");
-      }
-    }
-  }
+      // Continue game
+      if (state.round < TotalRounds) {
+        let CorrectAns = state.num1 + state.num2;   // Adds two numbers
+        if (parseInt(state.ans) == CorrectAns) {
+          // Call setState() -> Update the question
+          setState({
+            round: state.round + 1,
+            num1: Math.floor((Math.random() * 10) + 1),
+            num2: Math.floor((Math.random() * 10) + 1),
+            ans: "",
+            score: state.score + 1
+          });
 
-  // Return the component
+        } else {
+          // Call setState() -> Update the question
+          setState({
+            round: state.round + 1,
+            num1: Math.floor((Math.random() * 10) + 1),
+            num2: Math.floor((Math.random() * 10) + 1),
+            ans: "",
+            score: state.score
+          });
+        };
+      };
+
+      // When Final Round is Finished
+      if (state.round == TotalRounds) {
+        let CorrectAns = state.num1 + state.num2;
+        if (parseInt(state.ans) == CorrectAns) {
+          setState({
+            ...state,
+            round: "Game is Finished!",
+            score: state.score + 1
+          });
+        }
+        else {
+          setState({
+            ...state,
+            round: "Game is Finished!"
+          });
+        };
+
+        // TODO
+
+      };
+    };
+  };
+
+  // Main Quiz Window
   return (
     <div>
+      <h1>Round: {state.round}</h1>
       <div>{state.num1} + {state.num2}</div>
       <input value={state.ans} onChange={updateAnswer} onKeyPress={checkAnswer}
-       type="text" autoFocus/>
+        type="text" autoFocus />
+      <div>Score: {state.score}</div>
     </div>
   );
 }
---
+++ src/components/gameFinished.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const gameFinished = () => {
+    return (
+        <div>
+            <h1>Game Finished!</h1>
+        </div>
+    )
+}
+
+export default gameFinished
