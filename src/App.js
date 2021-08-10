82d74c9017369a8ec1b988984ce93305f9636910
Syed Shehroz Ali <sshehroza13@gmail.com>
2021-08-10 16:34:40 +0500

    Declared React useState()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
+
+  // Declare React state
+  const [state, setState] = React.useState({
+    num1: 1,
+    num2: 2,
+    ans: ""
+  })
+
+  // Update the input field what ever the user has typed in
+  function updateAnswer(event) {
+    setState({
+      ...state,
+      ans: event.target.value
+    });
+  }
+
+  // Check the user submitted Answer
+  function checkAnswer(event) {
+    if (event.key == "Enter") {
+      
+      let CorrectAns = state.num1 + state.num2;   // Adds two numbers
+      if (parseInt(state.ans) == CorrectAns) {
+        console.log("Answer is Correct!");
+
+        // Call setState()
+        setState({
+          num1: 5,
+          num2: 11,
+          ans: ""
+        });
+        
+      } else {
+        console.log("Answer is Wrong!");
+      }
+    }
+  }
+
+  // Return the component
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{state.num1} + {state.num2}</div>
+      <input value={state.ans} onChange={updateAnswer} onKeyPress={checkAnswer}
+       type="text" autoFocus/>
     </div>
   );
 }
