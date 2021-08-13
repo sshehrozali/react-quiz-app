eed8905c97e266c33af24020b76298cc24068f2a
Syed Shehroz Ali <sshehroza13@gmail.com>
2021-08-14 02:31:16 +0500

    Updated React App UI and Bootstrap5 classes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { stat } from 'fs';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import gameFinished from './components/gameFinished';
 import './App.css'
+import './css/normalLevel.css'
+import './css/mediumLevel.css'
+import './css/expertLevel.css'
+import './css/finishedLevel.css'
 
 function App() {
 
@@ -11,11 +14,12 @@ function App() {
 
   // Declare React state
   const [state, setState] = React.useState({
-    round: 1,
+    round: `Round: 1`,
     num1: 1,
     num2: 2,
     ans: "",
-    score: 0
+    score: 0,
+    difficulty: document.querySelector("body").classList.add("green")
   });
 
   // Update the input field what ever the user has typed in
@@ -26,113 +30,123 @@ function App() {
     });
   }
 
+  // Updates the Question
+  function updateQuestion(difficulty, level) {
+    let CurrentRound = parseInt(state.round[state.round.length - 1]);   // Findout Current Round
+    CurrentRound += 1;  // Increaments Curr Round by 1
+
+    let CorrectAns = state.num1 + state.num2;
+    if (parseInt(state.ans) == CorrectAns) {
+
+      // Call setState() -> Update the question (Increase Difficulty Level Also)
+      setState({
+        round: `Round: ${CurrentRound}`,
+        num1: Math.floor((Math.random() * `${difficulty}`) + 1),
+        num2: Math.floor((Math.random() * `${difficulty}`) + 1),
+        ans: "",
+        score: state.score + 1,
+        difficulty: document.querySelector("body").classList.add(`${level}`)
+      });
+    }
+
+    else {
+      setState({
+        round: `Round: ${CurrentRound}`,
+        num1: Math.floor((Math.random() * 10) + 1),
+        num2: Math.floor((Math.random() * 10) + 1),
+        ans: "",
+        score: state.score,
+        difficulty: document.querySelector("body").classList.add(`${level}`)
+      });
+    };
+  };
+
   // Check the user submitted Answer
   function checkAnswer(event) {
-
+    // If event is triggered
     if (event.key == "Enter") {
+      let CurrentRound;
+
+      if (state.round.length != 9) {
+        CurrentRound = parseInt(state.round[state.round.length - 1]);   // Findout Current Round
+        console.log(state.round.length);
+      }
+      if (state.round.length == 9) {
+        CurrentRound = 10;
+      }
 
       // Continue game
-      if (state.round < TotalRounds) {
+      if (CurrentRound < TotalRounds) {
+        console.log("In current round");
 
-        // For all rounds that comes after 4 (Increases Difficulty Level)
-        if (state.round > 4) {
-          let CorrectAns = state.num1 + state.num2;
-          if (parseInt(state.ans) == CorrectAns) {
+        // Round 8 - 10 (Expert Difficulty)
+        if (CurrentRound > 7) {
+          updateQuestion(1000, "red");
+        };
 
-            // Call setState() -> Update the question (Increase Difficulty Level Also)
-            setState({
-              round: state.round + 1,
-              num1: Math.floor((Math.random() * 100) + 1),
-              num2: Math.floor((Math.random() * 100) + 1),
-              ans: "",
-              score: state.score + 1
-            });
-          }
+        // Round 5 - 7 (Medium Difficulty)
+        if (CurrentRound > 4 && CurrentRound <= 7) {
+          updateQuestion(100, "blue");
+        };
 
-          else {
-            setState({
-              round: state.round + 1,
-              num1: Math.floor((Math.random() * 10) + 1),
-              num2: Math.floor((Math.random() * 10) + 1),
-              ans: "",
-              score: state.score
-            });
-
-
-          } //
-          } //
-
-        // For all rounds that comes or equal to 4 (Normal Difficulty Level)
-        if (state.round <= 4) {
-          let CorrectAns = state.num1 + state.num2;   // Adds two numbers
-          if (parseInt(state.ans) == CorrectAns) {
-            // Call setState() -> Update the question
-            setState({
-              round: state.round + 1,
-              num1: Math.floor((Math.random() * 10) + 1),
-              num2: Math.floor((Math.random() * 10) + 1),
-              ans: "",
-              score: state.score + 1
-            });
-
-          } else {
-            // Call setState() -> Update the question
-            setState({
-              round: state.round + 1,
-              num1: Math.floor((Math.random() * 10) + 1),
-              num2: Math.floor((Math.random() * 10) + 1),
-              ans: "",
-              score: state.score
-            });
-
-
-          } //
-
-        };//
-      
-      }; //
+        // Round 1 - 4 (Normal Difficulty)
+        if (CurrentRound <= 4) {
+          updateQuestion(10, "green");
+        };
+      }
 
       // When Final Round is Finished
-      if (state.round == TotalRounds) {
+      if (CurrentRound == TotalRounds) {
+        console.log("In Final Round")
+
         let CorrectAns = state.num1 + state.num2;
         if (parseInt(state.ans) == CorrectAns) {
           setState({
             ...state,
             round: "Game is Finished!",
-            score: state.score + 1
+            score: state.score + 1,
+            difficulty: document.querySelector("body").classList.add("end")
           });
         }
         else {
           setState({
             ...state,
-            round: "Game is Finished!"
+            round: "Game is Finished!",
+            difficulty: document.querySelector("body").classList.add("end")
           });
         };
 
         // TODO
 
-      };  //
+      };
 
-    }; //
+    };
 
-  }; //
+  };
 
   // Main Quiz Window
   return (
-    <body className="d-flex justify-content-center align-items-center">
+    <body className="d-flex flex-column justify-content-center align-items-center">
       <div className="container">
         <div className="row d-flex flex-column justify-content-center align-items-center">
           <div className="col-12 d-flex flex-column justify-content-center align-items-center">
-            <h1 className="display-4 text-dark mb-5">Round: {state.round}</h1>
-            <div className="mb-3 fs-4">Ques. {state.num1} + {state.num2} ?</div>
-            <input value={state.ans} onChange={updateAnswer} onKeyPress={checkAnswer}
-              type="text" autoFocus />
-            <div className="mt-5 fw-bold">Score: {state.score}</div>
+            <h1 className="display-4 text-white p-3 rounded mb-5 text-center round">{state.round}</h1>
+            <div className="mb-5 fs-4 text-white">Ques. {state.num1} + {state.num2} ?</div>
+
+              <div className="row d-flex flex-row justify-content-center">
+                <div className="col-12 d-flex flex-row justify-content-center">
+                  <input className="input text-white mx-1 mx-sm-1 mx-md-1 mx-lg-3 py-2 px-0 px-sm-0 px-md-0 px-lg-4 fs-4 text-center rounded mt-2" value={state.ans} onChange={updateAnswer} onKeyPress={checkAnswer}
+                    type="text" autoFocus />
+                  <button class="btn btn-success px-3 px-sm-3 px-md-3 px-lg-4 fs-1" type="button" id="button-addon2">✔</button>
+                </div>
+              </div>
+
+              <div className="score mt-5 fw-bold rounded" placeholder="Enter Your Answer Here">Score: {state.score}</div>
+            </div>
           </div>
         </div>
-      </div>
     </body>
-  );
+      );
 }
 
-export default App;
+      export default App;
---
+++ src/components/progress.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './progress.css'
+
+const progress = () => {
+    return (
+        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+    )
+}
+
+export default progress
